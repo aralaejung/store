d7be8e7136752b4fc54f6dcfa2c00023090bfdf4
Aralaejung <aralaejung@gmail.com>
2024-09-10 14:40:22 +0700

    product page components

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,13 +4,16 @@ import NavSearch from '@/components/navbar/NavSearch';
 import CartButton from '@/components/navbar/CartButton';
 import DarkMode from '@/components/navbar/DarkMode';
 import LinksDropdown from '@/components/navbar/LinksDropdown';
+import { Suspense } from 'react';
 
 function Navbar() {
   return (
     <nav className="border-b">
       <Container className="flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8">
         <Logo />
-        <NavSearch />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
         <div className="flex gap-4 items-center">
           <CartButton />
           <DarkMode />

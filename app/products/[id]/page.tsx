764605dc84ab01feacaf495f6d34f5015c7f6b36
Aralaejung <aralaejung@gmail.com>
2024-09-12 21:00:36 +0700

    review product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,15 +2,21 @@ import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import BreadCrumbs from '@/components/single-product/BreadCrumbs';
 import ProductRating from '@/components/single-product/ProductRating';
-import { fetchSingleProduct } from '@/utils/actions';
+import { fetchSingleProduct, findExistingReview } from '@/utils/actions';
 import { formatCurrency } from '@/utils/format';
 import Image from 'next/image';
 import ShareButton from '@/components/single-product/ShareButton';
+import ProductReviews from '@/components/reviews/ProductReviews';
+import SubmitReview from '@/components/reviews/SubmitReview';
+import { auth } from '@clerk/nextjs/server';
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
+  const { userId } = auth();
+  const reviewDoesNotExist =
+    userId && !(await findExistingReview(userId, product.id));
 
   return (
     <section>
@@ -29,14 +35,14 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
         </div>
         {/* PRODUCT INFO SECOND COL */}
         <div>
-          <div className="flex gap-x-8 items-center">
+          <div className="flexgap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
             <div className="flex items-center gap-x-2">
               <FavoriteToggleButton productId={params.id} />
               <ShareButton name={product.name} productId={params.id} />
             </div>
           </div>
-          <ProductRating />
+          <ProductRating productId={params.id} />
           <h4 className="text-xl mt-2">{company}</h4>
           <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
             {dollarsAmount}
@@ -45,6 +51,9 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
           <AddToCart />
         </div>
       </div>
+      <ProductReviews productId={params.id} />
+      {reviewDoesNotExist && <SubmitReview productId={params.id} />}
+      <SubmitReview productId={params.id} />
     </section>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'inrreoxuyxuldqytxhkl.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
     ],
   },
 };
